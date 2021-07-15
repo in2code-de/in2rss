31234bc9e2ebca879dbb07ee8304e0d80e04e8d0
Alex Kellner <alexander.kellner@in2code.de>
2021-07-15 12:30:31 +0200

    [TASK] Hide unneeded fields

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,9 +2,16 @@
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 defined('TYPO3_MODE') || die('Access denied.');
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['in2rss_main']='layout,select_key,pages';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['in2rss_main'] = 'pi_flexform';
+/**
+ * Hide unneeded fields
+ */
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['in2rss_main']
+    ='pages,recursive';
 
+/**
+ * Add FlexForm
+ */
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['in2rss_main'] = 'pi_flexform';
 ExtensionManagementUtility::addPiFlexFormValue(
     'in2rss_main',
     'FILE:EXT:in2rss/Configuration/FlexForm/Main.xml'
